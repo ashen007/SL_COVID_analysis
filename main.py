531650377f31f3bbce7205dfccd7db744c9b3fa5
ashen007 <asheniranga@gmail.com>
2021-05-19 12:22:54 +0530

    add daily reports pdfs

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from module import GetData
 if __name__ == '__main__':
     respond = GetData.getter()
     GetData.dump_json(respond, 'data/case_data.json')
+    GetData.get_daily_reports(
+        'https://www.epid.gov.lk/web/index.php?option=com_content&view=article&id=225&Itemid=518&lang=en',
+        path='data/daily_report')
--- module/GetData.py
+++ module/GetData.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 
 def getter():
@@ -12,3 +14,28 @@ def dump_json(responds, path):
             json.dump(responds.json(), file, sort_keys=True, indent=4)
     else:
         return responds.status_code
+
+
+def get_daily_reports(page, path):
+    base_url = page
+
+    if isinstance(base_url, str):
+        base_page = urlopen(base_url)
+        html = base_page.read().decode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+
+        host = 'https://www.epid.gov.lk'
+        table = soup.tbody
+        report_date = [trs.find_all('td')[0].text for trs in table.find_all('tr')[3:-2]]
+        report_url_anchors = [trs.find_all('td')[3].a for trs in table.find_all('tr')[3:-2]]
+        report_urls = [host + href['href'] for href in report_url_anchors]
+
+        for i in range(len(report_urls)):
+            sub_page = requests.get(report_urls[i])
+            file_path = path + '/sitrep{file_name}.pdf'.format(file_name=report_date[i])
+
+            with open(file_path, 'wb') as file:
+                file.write(sub_page.content)
+
+    else:
+        print('url error')
